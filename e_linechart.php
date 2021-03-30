20abb302b468b6ebe28d116fb45fa1784bfe03b3
JassiGhuman <jaskiratsingh211@gmail.com>
2021-03-31 03:16:23 +0530

    drop down added

--- e_linechart.php
+++ e_linechart.php
@@ -10,11 +10,10 @@
 // Define the variable and set to null by default
 $city_name = $category_name =  "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   
+if ($_SERVER["REQUEST_METHOD"] == "POST") {   
     $city_name = get_input($_POST["city_name"]);
     $category_name = get_input($_POST["category_name"]);
-
+    $c1 = get_input($_POST["c1"]);
 }
 
 function get_input($data) {
@@ -25,21 +24,88 @@ function get_input($data) {
 }
 ?>
 
+
+
+
+<?php
+
+function dynamic_select($the_array, $element_name, $label = '', $init_value = '') {
+    $menu = '';
+    if ($label != '') $menu .= '
+    	<label for="'.$element_name.'">'.$label.'</label>';
+    $menu .= '
+    	<select name="'.$element_name.'" id="'.$element_name.'">';
+    if (empty($_REQUEST[$element_name])) {
+        $curr_val = $init_value;
+    } else {
+        $curr_val = $_REQUEST[$element_name];
+    }
+    foreach ($the_array as $key => $value) {
+        $menu .= '
+			<option value="'.$key.'"';
+        if ($key == $curr_val) $menu .= ' selected="selected"';
+        $menu .= '>'.$value.'</option>';
+    }
+    $menu .= '
+    	</select>';
+    return $menu;
+}
+
+
+
+
+$cities=array("Vancouver", "Portland", 'San Francisco', "Seattle", "Los Angeles", "San Diego", "Las Vegas", "Phoenix", "Albuquerque", "Denver", "San Antonio"
+, "Dallas", "Houston", "Kansas City", "Minneapolis", "Saint Louis", "Chicago", "Nashville", "Indianapolis", "Atlanta", "Detroit", "Jacksonville", "Charlotte"
+, "Miami", "Pittsburgh", "Toronto", "Philadelphia", "New York", "Montreal", "Boston", "Beersheba", "Tel Aviv District", "Eilat", "Haifa", "Nahariyya");
+
+$categories = array ('humidity_per_day' => 'Humidity', 'pressure_per_day' => 'Pressure', 'temperature_per_day' => 'Temperature', 'wind_speed_per_day' => 'Wind Speed');
+
+$month = array (1=>"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");  
+$colors = array("red", "green", "blue", "yellow");
+$curr_month = date("m"); 
+//echo dynamic_select($month, 'month', 'Select a month', $curr_month);
+//echo dynamic_select($categories, '$category1', 'Choose Category', 'pressure_per_day');
+?>
+
+
+
+
+
 <!-- form to get user input -->
 <h2>Weather Wizard Vizualization</h2>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
    City Name: <input type="text" name="city_name">
+   <select name="c1" required>
+   <option value="SanDiego">San Diego</option>
+        <?php
+        foreach ($cities as $value) {
+        echo "<option value={$value}>$value</option>";
+        }?>
+    </select>
+
    <br><br>
-   Category Name: <input type="text" name="category_name">
+   Category Name: <!-- <input type="text" name="category_name"> -->
+   <select name="category_name" required>
+        <?php
+        foreach ($categories as $key => $value) {
+        echo "<option value=$key>$value</option>";
+        }?>
+    </select>
+
+
    <br><br>
    <input type="submit" name="submit" value="Submit"> 
 </form>
 
 <?php
 if ($city_name!=null && $category_name!=null){
-    echo "<h2>The input content is:</h2>";
+    echo "<h2>Selected City: ";
     echo $city_name;
+    echo " & Category: ";
     echo $category_name;
+    echo "</h2>";
+    echo $c1;
+    //$("#err").text("Login Failed")
     echo "<br>";
 
     //variable to connect database
